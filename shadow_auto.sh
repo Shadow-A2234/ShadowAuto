c9a26c3431dcf76bd3fcafd3a9aefaaaf7760e6b
Shadow-A2234 <amangzeneth@gmail.com>
2026-04-08 01:56:00 +0800

    Update Rin Code

--- shadow_auto.sh
+++ shadow_auto.sh
@@ -10,16 +10,16 @@ echo "Rin mengambil alih... Mengaktifkan unit taktis universal."
 sleep 1
 
 # --- TERMINAL 1: NMAP ---
-xfce4-terminal --geometry=130x35+50+50 --title="RECON_UNIT" -e "bash -c 'echo \"Scanning Ports...\"; nmap -sV -T4 $TARGET; exec bash'" &
+qterminal --geometry 1100x600+50+50 -e "bash -c 'echo \"Scanning Ports...\"; nmap -sV -T4 $TARGET; exec bash'" &
 
 sleep 1.5
 
 # --- TERMINAL 2: GOBUSTER ---
-xfce4-terminal --geometry=110x25+300+500 --title="DIRECTORY_FUZZER" -e "bash -c 'echo \"Fuzzing Directories...\"; gobuster dir -u http://$TARGET -w /usr/share/wordlists/dirb/common.txt; exec bash'" &
+qterminal --geometry 900x450+200+500 -e "bash -c 'echo \"Fuzzing Directories...\"; gobuster dir -u http://$TARGET -w /usr/share/wordlists/dirb/common.txt; exec bash'" &
 
 sleep 1
 
 # --- TERMINAL 3: NIKTO ---
-xfce4-terminal --geometry=80x20+1100+100 --title="VULN_SCANNER" -e "bash -c 'echo \"Checking Vulnerabilities...\"; nikto -h $TARGET; exec bash'" &
+qterminal --geometry 700x350+1100+100 -e "bash -c 'echo \"Checking Vulnerabilities...\"; nikto -h $TARGET; exec bash'" &
 
 echo "Sistem Run. Semua unit dikerahkan!"
