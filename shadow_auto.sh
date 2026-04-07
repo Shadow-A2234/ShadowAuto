32660cd04cedf07f1f06b58d97c6b9dc0a78a2c4
Shadow-A2234 <amangzeneth@gmail.com>
2026-04-08 01:46:38 +0800

    Update Rin Code

--- shadow_auto.sh
+++ shadow_auto.sh
@@ -10,16 +10,16 @@ echo "Rin mengambil alih... Mengaktifkan unit taktis universal."
 sleep 1
 
 # --- TERMINAL 1: NMAP ---
-x-terminal-emulator --geometry 100x25+50+50 -e bash -c "nmap -sV -T4 $TARGET; exec bash" &
+xfce4-terminal --geometry=130x35+50+50 --title="RECON_UNIT" -e "bash -c 'echo \"Scanning Ports...\"; nmap -sV -T4 $TARGET; exec bash'" &
 
 sleep 1.5
 
 # --- TERMINAL 2: GOBUSTER ---
-x-terminal-emulator --geometry 90x20+200+450 -e bash -c "gobuster dir -u http://$TARGET -w /usr/share/wordlists/dirb/common.txt; exec bash" &
+xfce4-terminal --geometry=110x25+300+500 --title="DIRECTORY_FUZZER" -e "bash -c 'echo \"Fuzzing Directories...\"; gobuster dir -u http://$TARGET -w /usr/share/wordlists/dirb/common.txt; exec bash'" &
 
 sleep 1
 
 # --- TERMINAL 3: NIKTO ---
-x-terminal-emulator --geometry 70x15+1200+100 -e bash -c "nikto -h $TARGET; exec bash" &
+xfce4-terminal --geometry=80x20+1100+100 --title="VULN_SCANNER" -e "bash -c 'echo \"Checking Vulnerabilities...\"; nikto -h $TARGET; exec bash'" &
 
 echo "Sistem Run. Semua unit dikerahkan!"
